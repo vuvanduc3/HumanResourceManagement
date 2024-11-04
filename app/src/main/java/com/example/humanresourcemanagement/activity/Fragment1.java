f0c59154034207f2d1861c82eaad9dcb8f59ce6f
huyvu06 <vuvanhuy020604@gmail.com>
2024-11-04 10:18:41 +0700

    a

--- app/src/main/java/com/example/humanresourcemanagement/activity/Fragment1.java
+++ app/src/main/java/com/example/humanresourcemanagement/activity/Fragment1.java
@@ -1,2 +1,22 @@
-package com.example.humanresourcemanagement.activity;public class Fragment1 {
+package com.example.humanresourcemanagement.activity;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.humanresourcemanagement.R;
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+        return view;
+    }
 }
