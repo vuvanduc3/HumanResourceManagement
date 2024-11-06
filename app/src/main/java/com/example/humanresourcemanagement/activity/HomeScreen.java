9caa801d09d20310ad4c5ecbc3f1419dc103af21
vuvanduc3 <vuvanduc.tdc.2223@gmail.com>
2024-11-06 07:34:26 +0700

    noi man hình home

--- app/src/main/java/com/example/humanresourcemanagement/activity/HomeScreen.java
+++ app/src/main/java/com/example/humanresourcemanagement/activity/HomeScreen.java
@@ -1,13 +1,31 @@
 package com.example.humanresourcemanagement.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.example.humanresourcemanagement.R;
-public class HomeScreen extends AppCompatActivity {
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.humanresourcemanagement.R;
+import com.example.humanresourcemanagement.databinding.ActivityEmployeeDetailBinding;
+import com.example.humanresourcemanagement.databinding.HomeLayoutBinding;
+
+public class HomeScreen extends AppCompatActivity {
+    private HomeLayoutBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_layout); // Gán layout ở đây
+        binding = HomeLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.lnNhanVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreen.this, EmployeeListActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
